ca28d27672bc5b784cac3f1148ef6ec87b90eae7
Helene Bloch <ln-45@live.fr>
2018-10-12 11:55:38 +0200

    SPRINT01_US02_DIAPORAMA section carousel et phrase d'accroche

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -1,15 +1,22 @@
 <?php
 /**
  * Created by PhpStorm.
+<<<<<<< HEAD
  * User: varloteaux
  * Date: 10/10/18
  * Time: 13:37
+=======
+ * User: ln
+ * Date: 12/10/18
+ * Time: 09:44
+>>>>>>> SPRINT01_US02_DIAPORAMA section carousel et phrase d'accroche
  */
 
 namespace Controller;
 
 class HomeController extends AbstractController
 {
+
     public function show()
     {
         return $this->twig->render('Home/home.html.twig', ["home" => "active"]);
