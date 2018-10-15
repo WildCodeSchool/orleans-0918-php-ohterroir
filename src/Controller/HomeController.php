fe1b9790a84690b0f4318a89d3f900d1c354f67d
open-it <nicolas.s.vincent@gmail.com>
2018-10-15 17:12:58 +0200

    Update HomeController.php

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -1,15 +1,9 @@
 <?php
 /**
  * Created by PhpStorm.
-<<<<<<< HEAD
- * User: varloteaux
- * Date: 10/10/18
- * Time: 13:37
-=======
  * User: ln
  * Date: 12/10/18
  * Time: 09:44
->>>>>>> SPRINT01_US02_DIAPORAMA section carousel et phrase d'accroche
  */
 
 namespace Controller;
