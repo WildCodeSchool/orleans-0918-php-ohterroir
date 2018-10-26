5c339d0a1287127046fd1428ccb03aeaf145dfb7
Amadou Kane <ak45amad@hotmail.fr>
2018-10-26 12:25:09 +0200

    #36_CORRECTIONS

--- src/Controller/GrowerController.php
+++ src/Controller/GrowerController.php
@@ -8,12 +8,23 @@
 
 namespace Controller;
 
+use Model\ContactDetailsManager;
+use Model\ScheduleManager;
+
 class GrowerController extends AbstractController
 {
     public function show()
     {
+        $contactManager = new ContactDetailsManager($this->getPdo());
+        $contacts = $contactManager->selectAll();
+
+        $scheduleManager = new ScheduleManager($this->getPdo());
+        $schedules = $scheduleManager->selectSchedule();
+
         return $this->twig->render('Grower/show.html.twig',[
             "growerPage" => "active",
+            "contacts" => $contacts,
+            "schedules" => $schedules,
         ]);
     }
 }
