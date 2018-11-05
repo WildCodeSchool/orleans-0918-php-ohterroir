ce91e67d605fd780955eff1bcc2a4549f0a3af70
Lucile Varloteaux <42941432+Luluribe45@users.noreply.github.com>
2018-11-05 17:27:00 +0100

    travis correction essai 3

--- src/Model/DishCategoryManager.php
+++ src/Model/DishCategoryManager.php
@@ -46,8 +46,9 @@ class DishCategoryManager extends AbstractManager
         $statement->bindValue(':namePageHome', $dishCategory->getNamePageHome(), \PDO::PARAM_STR);
         $statement->bindValue(':namePageDish', $dishCategory->getNamePageDish(), \PDO::PARAM_STR);
         $statement->bindValue(':description', $dishCategory->getDescription(), \PDO::PARAM_STR);
-        $statement->bindValue(':complementaryInformation', $dishCategory->getComplementaryInformation(),
-        \PDO::PARAM_STR);
+        $statement->bindValue(':complementaryInformation',
+                              $dishCategory->getComplementaryInformation(),
+                              \PDO::PARAM_STR);
         $statement->bindValue(':urlPictureForPageHome', '', \PDO::PARAM_STR);
         $statement->bindValue(':urlPictureForPageDish', '', \PDO::PARAM_STR);
         $statement->bindValue(':isActive', false, \PDO::PARAM_BOOL);
